b5f77a0337e94ec770741a0c6ce3da4d4411b2c8
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2016-03-13 13:23:23 -0500

    search Modulo Producto

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -99,4 +99,44 @@ class ProductController extends Controller {
 		return Redirect::to('admin/product')->with('success_message', 'El registro ha sido borrado.')->withInput();
 	}
 
+	////////////////////////////////////////////////////////////////////////////
+	// SECCION DE CODIGO PARA OTROS USOS
+	////////////////////////////////////////////////////////////////////////////
+	/**
+	 * Metodo para hacer la busqueda
+	 */
+	public static function search(Request $request) {
+			$items = array();
+			$search = '';
+			if ($request->input('search')) {
+					$search = $request->input('search');
+					$arrparam = explode(' ', $search);
+					$items = Product::whereNested(function($q) use ($arrparam) {
+							$p = $arrparam[0];
+							$q->whereNested(function($q) use ($p) {
+									$q->where('id', 'LIKE', '%' . $p . '%');
+									$q->orwhere('name', 'LIKE', '%' . $p . '%');
+									$q->orwhere('reference', 'LIKE', '%' . $p . '%');
+									$q->orwhere('enable', 'LIKE', '%' . $p . '%');
+							});
+							$c = count($arrparam);
+							if ($c > 1) {
+								for ($i = 1; $i < $c; $i++) {
+											$p = $arrparam[$i];
+											$q->whereNested(function($q) use ($p) {
+													$q->where('id', 'LIKE', '%' . $p . '%');
+													$q->orwhere('name', 'LIKE', '%' . $p . '%');
+													$q->orwhere('reference', 'LIKE', '%' . $p . '%');
+													$q->orwhere('enable', 'LIKE', '%' . $p . '%');
+											}, 'OR');
+									}
+							}
+					})
+					->whereNull('deleted_at')
+					->orderBy('name', 'ASC')
+					->paginate(10);
+					return View::make('admin.product.view_product', compact('items', 'search'));
+			}
+	}
+
 }
